5ce600b30e98caa1e7b10a220e7e32355fd76a51
Peter Kang <Peterrkang@gmail.com>
2017-02-25 18:21:43 -0800

    add whenever gem

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'rack-cors', :require => 'rack/cors'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
+# Use to update event models every 24 hours
+gem 'whenever', :require => false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,14 @@
 class EventsController < ApplicationController
 
   def index
-    events = Event.all
+    @current_time = Time.new
+    events = Event.where("created_at - @current_time > 4")
     render json: events
   end
 
   def create
     event = Event.create(title: params[:title], description: params[:description], address: params[:address], lat: params[:lat],
     lng: params[:lng], user_id: @current_user.id, showInfo: false)
-
     if event
       render json: { event: event }, status: 200
     else
