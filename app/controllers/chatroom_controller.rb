1fd355db341021ac533edf2816c51ed072982f73
Peter Kang <Peterrkang@gmail.com>
2017-03-07 16:24:12 -0800

    edit chatroom controller to find chatroom by event_id

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomController < ApplicationController
 
   def show
-    chat = ChatRoom.find(event_id: params[:id])
+    chat = ChatRoom.find_by(event_id: params[:id])
     user = @current_user.email
     render json: {messages: chat.messages, user: user, title: chat.title}
   end
